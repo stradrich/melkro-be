0da0eb7b08c2fd8b3bba1a5ecc8766ff2accb880
Aldrich Pinso <skrzypcedrich@gmail.com>
2023-08-30 14:24:06 +0800

    stripe webhook

--- routes/stripe.routes.js
+++ routes/stripe.routes.js
@@ -74,6 +74,10 @@ router.post('/create-checkout-session', async (req, res) => {
 });
 
 
+
+
+
+
 // Stripe Webhook 
 const crypto = require('crypto');
 const db = require('../models/Payment'); // Replace with your database module or configuration
@@ -82,6 +86,7 @@ const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
 router.post('/webhook', express.raw({ type: 'application/json' }), async (req, res) => {
   console.log('checkpoint 1');
+  // Retrieve the stripe-signature header from both req.get and req.headers. This header contains the expected signature sent by Stripe to verify the authenticity of the webhook event.
   const sigHeader = req.get('stripe-signature'); // Get the stripe-signature header
 
   const sig = req.headers['stripe-signature'];
@@ -89,6 +94,7 @@ router.post('/webhook', express.raw({ type: 'application/json' }), async (req, r
   let event;
   try {
     console.log('checkpoint 2');
+    // Construct event object from the request body, the calculated signature (sig), and the endpointSecret. This method verifies that the event is indeed sent by Stripe and hasn't been tampered with.
     event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
   } catch (err) {
     console.log('checkpoint 3');
@@ -97,15 +103,17 @@ router.post('/webhook', express.raw({ type: 'application/json' }), async (req, r
   }
 
   // Calculate the expected signature
-  // const payload = req.body;
+  // Payload is calculated by serializing the request body as a JSON string. This payload will be used to verify the signature.
   const payload = JSON.stringify(req.body);
   console.log('Original Payload:', payload);
+  // The crypto module is used to calculate the expected signature using the payload and the endpointSecret from your environment variables.
   const expectedSig = crypto
     .createHmac('sha256', endpointSecret)
     .update(payload)
     .digest('hex');
 
     console.log('checkpoint 4');
+    console.log("Payload:", payload);
     console.log('Calculated Signature:', expectedSig);
 
   // Compare the calculated signature with the header signature
@@ -115,6 +123,7 @@ router.post('/webhook', express.raw({ type: 'application/json' }), async (req, r
   }
 
   // Handle the event
+  // Inside the event switch case, the code checks if the event type is 'checkout.session.completed'. If it is, it means a payment was successfully completed, and the code proceeds to extract the payment details and handle the insertion of the payment data into the database.
   switch (event.type) {
     case 'checkout.session.completed':
       const checkoutSession = event.data.object;
